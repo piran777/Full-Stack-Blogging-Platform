245002f084faa9434bd68992558023f2fdcac67c
piran777 <paminul@uwo.ca>
2024-03-26 00:49:42 -0400

    added some blog content and a bit of styling

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,42 @@ function App() {
       </header>
 
 
+        <div className = "entry">
+          
+        <img src = "https://techcrunch.com/wp-content/uploads/2024/03/fisker-GettyImages.jpg?w=730&crop=1" alt = "" />
+          <h2>Fisker trading suspended by NYSE </h2>
+
+          <p>The New York Stock Exchange said Monday it will immediately suspend trading shares of EV startup Fisker and is moving to take the company off its stock exchange.
+
+The exchange said Monday that Fisker’s stock is “no longer suitable for listing” because of “abnormally low” price levels. The decision comes a month after Fisker was warned by the NYSE that its stock price had spent 30 days trading below $1, putting it out of compliance with the exchange’s rules.</p>
+        </div>
+
+        <div className = "entry">
+          
+          <img src = "https://techcrunch.com/wp-content/uploads/2024/03/fisker-GettyImages.jpg?w=730&crop=1" alt = "" />
+            <h2>Fisker trading suspended by NYSE </h2>
+  
+            <p>The New York Stock Exchange said Monday it will immediately suspend trading shares of EV startup Fisker and is moving to take the company off its stock exchange.
+
+The exchange said Monday that Fisker’s stock is “no longer suitable for listing” because of “abnormally low” price levels. The decision comes a month after Fisker was warned by the NYSE that its stock price had spent 30 days trading below $1, putting it out of compliance with the exchange’s rules. </p>
+          </div>
+  
+
+          <div className = "entry">
+          
+          <img src = "https://techcrunch.com/wp-content/uploads/2024/03/fisker-GettyImages.jpg?w=730&crop=1" alt = "" />
+            <h2>Fisker trading suspended by NYSE </h2>
+  
+            <p>The New York Stock Exchange said Monday it will immediately suspend trading shares of EV startup Fisker and is moving to take the company off its stock exchange.
+
+The exchange said Monday that Fisker’s stock is “no longer suitable for listing” because of “abnormally low” price levels. The decision comes a month after Fisker was warned by the NYSE that its stock price had spent 30 days trading below $1, putting it out of compliance with the exchange’s rules.</p>
+          </div>
+  
+  
+
+
+
+
 
     </main>
   );
