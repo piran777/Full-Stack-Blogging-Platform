1c7986db21974ad9ef0c6768287d8d9cdbbc0370
piran777 <paminul@uwo.ca>
2024-03-26 05:29:16 -0400

    Made mongo work

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,14 +1,32 @@
 import '../App.css';
+import {useState} from "react";
 export default function RegisterPage(){
+const [username, setUsername ] = useState('');
+const [password, setPassword ] = useState(''); 
 
+async function register(event){
+    event.preventDefault();
+
+    const response  = await fetch('http://localhost:4000/register', { //async func so we need await
+        method: 'POST',
+        body: JSON.stringify({username, password}),
+        headers: {'Content-Type' : 'application/json'},
+    });
+    
+
+}
     return(
-        <form className ="register">
+        <form className ="register" onSubmit = {register}>
             <h1>Register</h1>
-            <input type = "text" placeholder ="username" />
-            <input type = "password" placeholder ="password" />
+            <input type = "text" 
+            placeholder ="username" 
+            value = {username} 
+            onChange = {ev => setUsername(ev.target.value)}/>
+            <input type = "password" 
+            placeholder ="password" 
+            value = {password} 
+            onChange = {ev => setPassword(ev.target.value)}/>
             <button> Register</button>
-
-
             </form>
     );
 }
